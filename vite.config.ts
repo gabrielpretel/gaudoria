75b3a6b48e75181cd68288f8b6b63fdb97c14fef
Gabriel Pretel <gabrilinex@gmail.com>
2024-11-06 11:52:11 +0100

    feat: update vite.config with legacy and checker plugins, add inline sourcemaps and HMR settings
    - Modified `vite.config.js` to include:
      - `vite-plugin-checker` for TypeScript checks.
      - `@vitejs/plugin-legacy` for compatibility with older browsers (excluding IE 11).
      - CSS module `localsConvention` set to "camelCase".
      - Inline sourcemaps for easier debugging.
      - HMR deactivated on dev server.
    - Added path alias "@" pointing to "src" directory.
    - Installed `react-router-dom` dependency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import legacy from "@vitejs/plugin-legacy";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,20 @@ export default defineConfig({
       targets: ["defaults", "not IE 11"],
     }),
   ],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    hmr: false,
+  },
+  build: {
+    sourcemap: "inline",
+  },
 });
